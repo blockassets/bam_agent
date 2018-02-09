1423ddca1e9cce67c3eb10df0069a0c79b1a42c0
Mat Henshall <mathenshall@gmail.com>
2018-02-09 16:39:27 +0700

    Refactor commands into a service layer

--- controller/cgminer_quit.go
+++ controller/cgminer_quit.go
@@ -1,11 +1,10 @@
 package controller
 
 import (
-	"log"
 	"net/http"
-	"time"
 
-	"github.com/blockassets/cgminer_client"
+	"github.com/blockassets/bam_agent/service"
+
 )
 
 // Implements Controller interface
@@ -22,12 +21,11 @@ func (c CGQuitCtrl) build() *Controller {
 func (c CGQuitCtrl) makeHandler() http.HandlerFunc {
 	return makeJsonHandler(
 		func(w http.ResponseWriter, r *http.Request) {
-			log.Printf("CGMiner Quit Requested")
-
 			bamStat := BAMStatus{"OK", nil}
 			httpStat := http.StatusOK
 
-			err := cgmQuit()
+			cmds := service.Command{}
+			err := cmds.CgmQuit()
 			if err != nil {
 				httpStat = http.StatusBadGateway
 				bamStat = BAMStatus{"Error", err}
@@ -36,9 +34,4 @@ func (c CGQuitCtrl) makeHandler() http.HandlerFunc {
 			resp, _ := json.Marshal(bamStat)
 			w.Write(resp)
 		})
-}
-
-func cgmQuit() error {
-	clnt := cgminer_client.New("localhost", 4028, 5*time.Second)
-	return clnt.Quit()
 }
--- monitor/monitor_load.go
+++ monitor/monitor_load.go
@@ -1,16 +1,18 @@
 package monitor
 
 import (
-	"github.com/blockassets/bam_agent/controller"
 	"log"
 	"time"
+
+	"github.com/blockassets/bam_agent/service"
 )
 
 func monitorLoad(sr statRetriever, interval time.Duration) {
 	for {
 		high, err := checkLoadAvg(sr)
 		if (err == nil) && high {
-			controller.Reboot()
+			cmds := service.Command{}
+			cmds.Reboot()
 		}
 		time.Sleep(interval)
 	}
