fc76a9852c4b845832362fe4fc13bfec0fe31006
Mat Henshall <mathenshall@gmail.com>
2018-02-26 11:45:41 +0700

    Version banner moved to first thing app does

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var (
 )
 
 func main() {
+	log.Printf("%s %s", os.Args[0], version)
 	monitor.StartMonitors()
 	startServer()
 }
@@ -38,7 +39,6 @@ func startServer() {
 
 	controller.Init(e)
 
-	log.Printf("%s %s", os.Args[0], version)
 
 	// Start server
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", *port)))
