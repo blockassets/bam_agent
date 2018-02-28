3460ad46d738f9b502b2fd40c3cba36906226f17
mat henshall <mathenshall@gmail.com>
2018-02-28 14:19:22 +0700

    Refactored reboot to make the delay before rebooting applied in the http handler.

--- service/reboot.go
+++ service/reboot.go
@@ -6,13 +6,9 @@ import (
 	"time"
 )
 
-const (
-	DELAY_BEFORE_REBOOT = 5 * time.Second
-)
+
 
 func Reboot() {
-	// Give enough time for any calls to return to client before rebooting
-	time.Sleep(DELAY_BEFORE_REBOOT)
 	log.Printf("Reboot Requested")
 	exec.Command("/sbin/reboot", "-f").Run()
 }
