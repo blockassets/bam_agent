d963f22245642fc5cb8f4a665fbc2102e4cfed79
Jon Stevens <latchkey@gmail.com>
2018-03-01 18:00:06 +0700

    enable a javascript page to talk to the server

--- controller/controller.go
+++ controller/controller.go
@@ -42,6 +42,11 @@ func makeJsonHandler(handler http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("Pragma", "no-cache")
 		w.Header().Set("Expires", "0")
 
+		// Allow any origin to hit the miner. Potential danger here, but if someone has access
+		// to your miner network, then you've got bigger problems anyway since they have a default
+		// root password (and some machines even have telnet enabled).
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
 		handler.ServeHTTP(w, r)
 	}
 }
