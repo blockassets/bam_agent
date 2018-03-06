1ff70a4b72468a48767c3b11269a4d81c1c18eca
mat henshall <mathenshall@gmail.com>
2018-03-06 16:33:42 +0700

    pulled constant out of test file, typo's fixed

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+const (
+	MonitorCycleDuration = time.Duration(50) * time.Millisecond
+)
+
 type TestMonitor struct {
 	*Context
 	CountIt func()
@@ -18,7 +22,7 @@ func newTestMonitor(context *Context, doItFunc func()) *TestMonitor {
 
 func (monitor *TestMonitor) Start() error {
 
-	go monitor.makeTimerFunc(monitor.CountIt, time.Duration(50)*time.Millisecond)()
+	go monitor.makeTimerFunc(monitor.CountIt, MonitorCycleDuration)()
 
 	return nil
 }
@@ -38,19 +42,18 @@ func (mgr *testManager) StartMonitors() {
 	for _, monitor := range *mgr.Monitors {
 		monitor.Start()
 	}
-
 }
 
 func TestManager_StopMonitors(t *testing.T) {
 	monitorManager := &testManager{Manager{Config: nil, Client: nil}}
-	
+
 	monitorManager.StopMonitors()
 	monitorManager.StartMonitors()
 	// let enough time to go through 1 cycle
-	time.Sleep(time.Duration(75) * time.Millisecond)
+	time.Sleep(MonitorCycleDuration * time.Duration(2))
 	monitorManager.StopMonitors()
 	monitorManager.StopMonitors()
 	if count != 1 {
-		t.Errorf("Expected Count to be 1, got %v", count)
+		t.Errorf("Expected count to be 1, got %v", count)
 	}
 }
