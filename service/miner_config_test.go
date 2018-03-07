98568bd9a1b0b25d437cb6d6713aad52e7637f1f
mat henshall <mathenshall@gmail.com>
2018-03-07 16:59:24 +0700

    Added tests for mutating the IPAddress in the miner config and also simplified and unified the testing of mutating for both pools and ipaddresses

--- service/miner_config_test.go
+++ service/miner_config_test.go
@@ -7,15 +7,8 @@ import (
 	"github.com/json-iterator/go"
 )
 
-var updatePoolsTests = []struct {
-	configFile  string
-	poolsIn     string
-	shouldErr   bool
-	expectedOut string
-}{
-	// happy path
-	{
-		configFile: `{
+const (
+	inputConfig = `{
 	"api-allow": "W:0/0",
 	"api-listen": true,
 	"api-port": "4028",
@@ -55,9 +48,9 @@ var updatePoolsTests = []struct {
 	"temp_threshold": "80",
 	"username": "admin",
 	"volt": "2"
-}`,
-		poolsIn: `{"pool1": "333.2.3.4", "pool2": "333.3.4.5", "pool3": "333.4.5.6"}`,
-		expectedOut: `{
+}`
+	poolsToMutate            = `{"pool1": "333.2.3.4", "pool2": "333.3.4.5", "pool3": "333.4.5.6"}`
+	expectedOutputPoolMutate = `{
 	"api-allow": "W:0/0",
 	"api-listen": true,
 	"api-port": "4028",
@@ -97,27 +90,67 @@ var updatePoolsTests = []struct {
 	"temp_threshold": "80",
 	"username": "admin",
 	"volt": "2"
-}`},
-}
+}`
+	IPAddressesToMutate           = `{ "address": "1.2.3.4", "netmask": "5.6.7.8", "gateway": "9.10.11.12", "dns": "13.13.13.13"}`
+	expectedOutputIPAddressMutate = `{
+	"api-allow": "W:0/0",
+	"api-listen": true,
+	"api-port": "4028",
+	"autoFrequency": true,
+	"autoGetJobTimeOut": true,
+	"autoNet": false,
+	"board_reenable_waittime": "60",
+	"board_reset_waittime": "14",
+	"botelv": true,
+	"chipNumber": "36",
+	"debug": true,
+	"dns": "13.13.13.13",
+	"failover-only": true,
+	"fanSet": "30_1000|34_2000|38_3000|42_4000|46_5000|50_6000",
+	"fengchu": "5000",
+	"fengru": "5000",
+	"frequency": "684",
+	"frequencySet": "384_30|450_30|480_30|540_30|576_30|600_30|612_30|625_30|636_30|648_30|660_29|672_29|684_28|700_28|720_28|744_28|756_28|768_28|800_28|912_28|1020_28",
+	"gateway": "9.10.11.12",
+	"invalid_cnt": "30",
+	"ip": "1.2.3.4",
+	"language": "ch",
+	"mask": "5.6.7.8",
+	"mcu_reset_waittime": "0",
+	"no-submit-stale": true,
+	"packet": true,
+	"password": "bw.com",
+	"pool1": "111.2.3.4",
+	"pool2": "111.3.4.5",
+	"pool3": "111.4.5.6",
+	"running_voltage1": "5650",
+	"running_voltage2": "5650",
+	"running_voltage3": "5650",
+	"scanwork_sleeptime": "4",
+	"start_voltage": "6000",
+	"task_interval": "350",
+	"temp_threshold": "80",
+	"username": "admin",
+	"volt": "2"
+}`
+)
 
 func TestMutatePools(t *testing.T) {
-	for index, tt := range updatePoolsTests {
-		jsonConfig, err := gabs.ParseJSON([]byte(tt.configFile))
-		if err != nil {
-			t.Error(err)
-		}
+	jsonConfig, err := gabs.ParseJSON([]byte(inputConfig))
+	if err != nil {
+		t.Error(err)
+	}
 
-		pools := &PoolAddresses{}
-		err = jsoniter.Unmarshal([]byte(tt.poolsIn), pools)
-		if err != nil {
-			t.Error(err)
-		}
+	pools := &PoolAddresses{}
+	err = jsoniter.Unmarshal([]byte(poolsToMutate), pools)
+	if err != nil {
+		t.Error(err)
+	}
 
-		mutated := mutatePools(pools, jsonConfig)
-		buf := string(mutated)
-		if tt.expectedOut != buf {
-			t.Errorf("Test Index: %v: Expected:\n%s\nGot:\n%s\n ", index, tt.expectedOut, buf)
-		}
+	mutated := mutatePools(pools, jsonConfig)
+	buf := string(mutated)
+	if expectedOutputPoolMutate != buf {
+		t.Errorf("Expected:\n%s\nGot:\n%s\n ", expectedOutputPoolMutate, buf)
 	}
 }
 
@@ -136,5 +169,36 @@ func TestUpdatePools(t *testing.T) {
 func TestConfigFilePath(t *testing.T) {
 	if configFilePath != "/usr/app/conf.default" {
 		t.Errorf("Expected config file path to be /usr/app/conf.default and got %s", configFilePath)
+	}
+}
+
+func TestUpdateIPAddresses(t *testing.T) {
+	err := UpdateIPAddresses(nil)
+	if err == nil {
+		t.Error("Should have had an error on nil input")
+	}
+
+	err = UpdateIPAddresses([]byte("{ this is bad json }"))
+	if err == nil {
+		t.Error("Should have had an error on bad input")
+	}
+}
+
+func TestMutateIPAddresses(t *testing.T) {
+	jsonConfig, err := gabs.ParseJSON([]byte(inputConfig))
+	if err != nil {
+		t.Error(err)
+	}
+
+	ips := &IPAddresses{}
+	err = jsoniter.Unmarshal([]byte(IPAddressesToMutate), ips)
+	if err != nil {
+		t.Error(err)
+	}
+
+	mutated := mutateIPAddresses(ips, jsonConfig)
+	buf := string(mutated)
+	if expectedOutputIPAddressMutate != buf {
+		t.Errorf("Expected:\n%s\nGot:\n%s\n ", expectedOutputIPAddressMutate, buf)
 	}
 }
