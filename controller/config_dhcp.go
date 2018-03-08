6e5d350a48e1e96f7cd69a326bfd0cb27c2e0e9c
mat henshall <mathenshall@gmail.com>
2018-03-08 13:39:08 +0700

    Added Handler for PUT config/dhcp

---
+++ controller/config_dhcp.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/blockassets/bam_agent/monitor"
+	"github.com/blockassets/bam_agent/service"
+	"github.com/json-iterator/go"
+)
+
+// Implements Builder interface
+type PutDhcpCtrl struct {
+	monitorManager *monitor.Manager
+}
+
+func (ctrl PutDhcpCtrl) build(cfg *Config) *Controller {
+	ctrl.monitorManager = cfg.MonitorManager
+
+	return &Controller{
+		Methods: []string{http.MethodPut},
+		Path:    "/config/dhcp",
+		Handler: ctrl.makeHandler(),
+	}
+}
+
+func (ctrl PutDhcpCtrl) makeHandler() http.HandlerFunc {
+	return makeJsonHandler(
+		func(w http.ResponseWriter, r *http.Request) {
+			bamStat := BAMStatus{"OK", nil}
+			httpStat := http.StatusOK
+
+			ctrl.monitorManager.StopMonitors()
+
+			err := service.UpdateDHCPNetConfig()
+			if err != nil {
+				httpStat = http.StatusBadGateway
+				bamStat = BAMStatus{"Error", err}
+			}
+
+			ctrl.monitorManager.StartMonitors()
+
+			w.WriteHeader(httpStat)
+			resp, _ := jsoniter.Marshal(bamStat)
+			w.Write(resp)
+		})
+}
