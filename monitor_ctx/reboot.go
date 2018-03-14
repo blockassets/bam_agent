48c6088dfba8f6e6950126a484f918d09fda74de
mat henshall <mathenshall@gmail.com>
2018-03-14 15:21:36 +0700

    Added periodic reboot monitor

---
+++ monitor_ctx/reboot.go
@@ -0,0 +1,23 @@
+package monitor_ctx
+
+import (
+	"context"
+	"log"
+
+	"github.com/blockassets/bam_agent/tool"
+)
+
+type RebootConfig struct {
+	Enabled bool                `json:"enabled"`
+	Period  tool.RandomDuration `json:"period"`
+}
+
+
+
+func NewPeriodicReboot(config *RebootConfig, rebootFunc func()) Monitor {
+	log.Printf("PeriodicRebootMonitor(enabled == %v): reboot in %v ",config.Enabled, config.Period.Duration)
+	monitorFunc := func(ctx context.Context) {
+		rebootFunc()
+	}
+	return &Periodic{config.Enabled, config.Period.Duration, monitorFunc}
+}
---
+++ monitor_ctx/reboot_test.go
@@ -0,0 +1,29 @@
+package monitor_ctx
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/blockassets/bam_agent/tool"
+)
+
+func TestPeriodicRebootMonitor_Start(t *testing.T) {
+	count := 0
+
+	config := &RebootConfig{Enabled: true, Period: tool.RandomDuration{Duration: time.Duration(25) * time.Millisecond}}
+	reboot := func() { count++ }
+
+	monitors := &[]Monitor{
+		NewPeriodicReboot( config, reboot),
+	}
+	stopMonitors := StartMonitors(context.Background(), *monitors)
+
+	// Sleep to ensure the timer runs once
+	time.Sleep(config.Period.Duration * 2)
+
+	stopMonitors()
+	if count == 0 {
+		t.Errorf("Expected >=1 count, got %d", count)
+	}
+}
