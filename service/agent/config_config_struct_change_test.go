b8b2a2e79c507315d82264d9415f37b9f1eb7b7c
mat henshall <mathenshall@gmail.com>
2018-03-18 18:18:23 +0700

    tst for a change in structure for agent config across binary releases

---
+++ service/agent/config_config_struct_change_test.go
@@ -0,0 +1,55 @@
+package agent
+
+import (
+"io/ioutil"
+"os"
+"testing"
+
+
+
+
+"github.com/blockassets/bam_agent/tool"
+
+)
+
+// simulate a BAM agent binary update that adds a structure to the defualt BAM interface
+// by saving a previous config file that doesnt have the current monitors in it
+//
+
+const prior_config_version =
+	`{
+	"cmdLine": {
+		"port": ":1111"
+	},
+	"controller": {
+		"reboot": {
+			"delay": "5s"
+		}
+	}
+}
+`
+
+func TestStructChangeToConfig(t *testing.T) {
+	file, err := ioutil.TempFile("/tmp", "agent-config")
+	defer os.Remove(file.Name())
+	fileName := file.Name()
+	file.Write([]byte( prior_config_version))
+	file.Close()
+
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cfg := NewConfig(tool.CmdLine{
+		AgentConfigPath: fileName,
+	})
+
+	if !cfg.Loaded().Monitor.HighLoad.Enabled {
+		t.Fatalf("expected highLoad to be enabled")
+	}
+
+}
+
+
+
