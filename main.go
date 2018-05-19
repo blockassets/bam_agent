a284f2759850c71705bd860dd8eb134669a3800c
Jon Stevens <latchkey@gmail.com>
2018-05-19 16:48:25 +0700

    switch to my branch of overseerer

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/blockassets/bam_agent/service/miner/cgminer"
 	"github.com/blockassets/bam_agent/service/os"
 	"github.com/blockassets/bam_agent/tool"
-	"github.com/jpillora/overseer"
+	"github.com/lookfirst/overseer"
 	"go.uber.org/fx"
 )
 
